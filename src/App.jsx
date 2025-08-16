68dc7d9c11fd2b49fba4cdf88b6e56cc6bda4f91
Deepankumar <deepan.guna12@gmail.com>
2025-08-16 11:17:30 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import './app.scss'
-import Contact from './components/contact/Contact';
-import Hero from './components/hero/Hero';
-import Navbar from './components/navbar/Navbar';
-import Parallex from './components/parallex/Parallex';
-import Portfolio from './components/portfolio/Portfolio';
+import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallex from "./components/parallex/Parallex";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
-  return <div>
-    <section id='Homepage'>
-      <Navbar/>
-      <Hero/>
-    </section>
-    <section id='Projects'>
-      <Parallex/>
-    </section>
-    <Portfolio/>
-    <section id='Contact'>
-      <Contact/>
-    </section>
-
-  </div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Projects">
+        <Parallex />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
 export default App;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,76 +1,88 @@
-import './hero.scss'
-import React from 'react'
-import { motion} from "framer-motion"
+import "./hero.scss";
+import React from "react";
+import { motion } from "framer-motion";
 
-
-
-const textVariants={
-    initial:{
-        opacity:0,
-        x:-500,
+const textVariants = {
+  initial: {
+    opacity: 0,
+    x: -500,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
     },
-    animate:{
-        opacity:1,
-        x:0,
-        transition:{
-            duration:1,
-            staggerChildren:0.1,
-        },
+  },
+  scrollButton: {
+    y: 10,
+    opacity: 0,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
     },
-    scrollButton:{
-        y:10,
-        opacity:0,
-        transition:{
-            duration:2,
-            repeat:Infinity,
-        },
-    },
-}
-const sliderVariants={
-    initial:{
-        x:0,
-    },
-    animate:{
-        x:"-335%",
-        transition:{
-            repeat:Infinity,
-            repeattype:"mirror",
-            duration:25,
-        },
-    },
-  
+  },
 };
-
-
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-335%",
+    transition: {
+      repeat: Infinity,
+      repeattype: "mirror",
+      duration: 25,
+    },
+  },
+};
 
 const Hero = () => {
   return (
-     <div className="hero">
-        <div className="wrapper">
-            <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate" >
-                <motion.h2 variants={textVariants}>DEEPANKUMAR G</motion.h2>
-                <motion.h1 variants={textVariants}>Full Stack Web Developer</motion.h1>
-                <motion.div className="buttons" variants={textVariants}>
-                    <motion.button variants={textVariants}> <a href="#Projects">See the latest Works</a> </motion.button>
-                    <motion.button variants={textVariants}> <a href="#Contact">Contact Me</a> </motion.button>
-                </motion.div>
-                <motion.img variants={textVariants} animate="scrollButton" src="./scroll.png" alt="" />
-            </motion.div>
-            <motion.div className="slidingContainer" 
-                variants={sliderVariants} 
-                initial="initial" 
-                animate="animate">
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>DEEPANKUMAR G</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Full Stack Web Developer
+          </motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              {" "}
+              <a href="#Projects">See the latest Works</a>{" "}
+            </motion.button>
+            <motion.button variants={textVariants}>
+              {" "}
+              <a href="#Contact">Contact Me</a>{" "}
+            </motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="./scroll.png"
+            alt=""
+          />
+        </motion.div>
+        <motion.div
+          className="slidingContainer"
+          variants={sliderVariants}
+          initial="initial"
+          animate="animate"
+        >
+          Full Stack Web Developer
+        </motion.div>
+      </div>
+      <div className="imgContainer">
+        <img src="/Deepan.png" alt="" />
+      </div>
+    </div>
+  );
+};
 
-                Full Stack Web Developer
-
-            </motion.div>
-        </div>
-        <div className="imgContainer">
-          
-                <img src="/Deepan.png" alt="" />
-        </div>
-     </div>
-  )
-}
-
-export default Hero
+export default Hero;
